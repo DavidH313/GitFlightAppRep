13ab5266bd612350761ce1babbe39f1367f2d74e
SorryLearner313 <2004david121@gmail.com>
2025-08-29 22:06:49 -0600

    Fase1

---
+++ Models/FlightConnection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFlightApp.Models
+{
+    public class FlightConnection
+    {
+        public string ConnectionId { get; set; } = string.Empty;
+
+        public string Airline { get; set; } = string.Empty;
+
+        public string DepartureCity {  get; set; } = string.Empty;
+
+        public string ArrivalCity {  get; set; } = string.Empty;
+
+        public ObservableCollection<Flight> Flights { get; set; } = [];
+    }
+}
---
+++ Models/GlobalData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFlightApp.Models
+{
+    public class GlobalData
+    {
+        private static ObservableCollection<Flight> flights = [new Flight()
+        {
+            FlightNumber = "LH123",
+            Price = 900,
+            DepartureDate = DateTime.Now,
+        },
+            new()
+            {
+            FlightNumber = "LH456",
+            Price = 7600,
+            DepartureDate = DateTime.Now,
+            },
+
+            new()
+            {
+            FlightNumber = "AB890",
+            Price = 300,
+            DepartureDate = DateTime.Now,
+            }
+
+            ];
+
+        public static ObservableCollection<FlightConnection> flightConnections = [
+            new()
+            {
+                Airline = "Iberia",
+                DepartureCity = "Chicago",
+                ConnectionId = "A14",
+                ArrivalCity = "New york",
+                Flights = []
+
+            }
+            ];
+        public static ObservableCollection<Flight> Flights { get => flights; set => flights = value; }
+        public static ObservableCollection<FlightConnection> FlightConnections { get => flightConnections; set => flightConnections = value; }
+
+    }
+}
